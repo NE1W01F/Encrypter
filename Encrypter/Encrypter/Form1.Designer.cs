a78ddf3e6bc839ac5a97bdc4351babb7ea796afb
Wolf <36646784+NE1W01F@users.noreply.github.com>
2018-06-19 21:22:08 +0100

    Fixed bugs
    - It now checks if the file exists
    - changed the look of it

--- Encrypter/Encrypter/Form1.Designer.cs
+++ Encrypter/Encrypter/Form1.Designer.cs
@@ -38,15 +38,15 @@ namespace Encrypter
             this.label3 = new System.Windows.Forms.Label();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.label4 = new System.Windows.Forms.Label();
-            this.label5 = new System.Windows.Forms.Label();
-            this.textBox3 = new System.Windows.Forms.TextBox();
             this.label6 = new System.Windows.Forms.Label();
             this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(500, 186);
+            this.button1.Location = new System.Drawing.Point(255, 128);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 1;
@@ -56,7 +56,7 @@ namespace Encrypter
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(12, 186);
+            this.button2.Location = new System.Drawing.Point(12, 128);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(75, 23);
             this.button2.TabIndex = 2;
@@ -66,7 +66,7 @@ namespace Encrypter
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(93, 186);
+            this.button3.Location = new System.Drawing.Point(93, 128);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(75, 23);
             this.button3.TabIndex = 3;
@@ -77,7 +77,7 @@ namespace Encrypter
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(-2, 15);
+            this.label1.Location = new System.Drawing.Point(2, 9);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(160, 13);
             this.label1.TabIndex = 4;
@@ -86,7 +86,7 @@ namespace Encrypter
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(-1, 137);
+            this.label2.Location = new System.Drawing.Point(2, 96);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(109, 13);
             this.label2.TabIndex = 5;
@@ -95,7 +95,7 @@ namespace Encrypter
             // 
             // button4
             // 
-            this.button4.Location = new System.Drawing.Point(182, 186);
+            this.button4.Location = new System.Drawing.Point(174, 128);
             this.button4.Name = "button4";
             this.button4.Size = new System.Drawing.Size(75, 23);
             this.button4.TabIndex = 6;
@@ -105,7 +105,7 @@ namespace Encrypter
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(114, 137);
+            this.textBox2.Location = new System.Drawing.Point(117, 93);
             this.textBox2.MaxLength = 8;
             this.textBox2.Name = "textBox2";
             this.textBox2.Size = new System.Drawing.Size(96, 20);
@@ -115,7 +115,7 @@ namespace Encrypter
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(309, 9);
+            this.label3.Location = new System.Drawing.Point(392, 7);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(275, 75);
             this.label3.TabIndex = 9;
@@ -125,7 +125,7 @@ namespace Encrypter
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(161, 12);
+            this.textBox1.Location = new System.Drawing.Point(168, 6);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(96, 20);
             this.textBox1.TabIndex = 10;
@@ -134,33 +134,17 @@ namespace Encrypter
             // 
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.Location = new System.Drawing.Point(309, 107);
+            this.label4.Location = new System.Drawing.Point(392, 93);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(198, 30);
             this.label4.TabIndex = 11;
             this.label4.Text = "Note: Your password can only be 8 \r\ncharacters long.";
             this.label4.Click += new System.EventHandler(this.label4_Click);
             // 
-            // label5
-            // 
-            this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(-1, 52);
-            this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(192, 13);
-            this.label5.TabIndex = 12;
-            this.label5.Text = "Enter the file name without the file type:";
-            // 
-            // textBox3
-            // 
-            this.textBox3.Location = new System.Drawing.Point(197, 49);
-            this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(100, 20);
-            this.textBox3.TabIndex = 13;
-            // 
             // label6
             // 
             this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(-1, 90);
+            this.label6.Location = new System.Drawing.Point(2, 48);
             this.label6.Name = "label6";
             this.label6.Size = new System.Drawing.Size(114, 13);
             this.label6.TabIndex = 14;
@@ -168,20 +152,37 @@ namespace Encrypter
             // 
             // textBox4
             // 
-            this.textBox4.Location = new System.Drawing.Point(119, 87);
+            this.textBox4.Location = new System.Drawing.Point(122, 45);
             this.textBox4.Name = "textBox4";
             this.textBox4.Size = new System.Drawing.Size(49, 20);
             this.textBox4.TabIndex = 15;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(104, 35);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(0, 13);
+            this.label5.TabIndex = 17;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(177, 48);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(209, 13);
+            this.label8.TabIndex = 18;
+            this.label8.Text = "only enter this in  if you are decrypting a file";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(587, 215);
+            this.ClientSize = new System.Drawing.Size(665, 158);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label5);
             this.Controls.Add(this.textBox4);
             this.Controls.Add(this.label6);
-            this.Controls.Add(this.textBox3);
-            this.Controls.Add(this.label5);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.label3);
@@ -210,10 +211,10 @@ namespace Encrypter
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.Label label5;
-        private System.Windows.Forms.TextBox textBox3;
         private System.Windows.Forms.Label label6;
         private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label8;
     }
 }
 
--- Encrypter/Encrypter/Form1.cs
+++ Encrypter/Encrypter/Form1.cs
@@ -25,61 +25,77 @@ namespace Encrypter
 
         private void button2_Click(object sender, EventArgs e)
         {
-            string inputFile = textBox1.Text;
-            string outputFile = textBox3.Text + ".cry";
-            string password = textBox2.Text;
-            UnicodeEncoding UE = new UnicodeEncoding();
-            byte[] key = UE.GetBytes(password);
+            if (textBox1.Text == textBox1.Text + ".cry")
+            {
+                MessageBox.Show("You can't encrypt a file that has already been encrypted.", "Encrypter", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else
+            {
+                string inputFile = textBox1.Text;
+                string outputFile = "Encrypted" + ".cry";
+                string password = textBox2.Text;
+                UnicodeEncoding UE = new UnicodeEncoding();
+                byte[] key = UE.GetBytes(password);
 
-            string cryptFile = outputFile;
-            FileStream fsCrypt = new FileStream(cryptFile, FileMode.Create);
+                string cryptFile = outputFile;
+                FileStream fsCrypt = new FileStream(cryptFile, FileMode.Create);
 
-            RijndaelManaged RMCrypto = new RijndaelManaged();
+                RijndaelManaged RMCrypto = new RijndaelManaged();
 
-            CryptoStream cs = new CryptoStream(fsCrypt,
-                RMCrypto.CreateEncryptor(key, key),
-                CryptoStreamMode.Write);
+                CryptoStream cs = new CryptoStream(fsCrypt,
+                    RMCrypto.CreateEncryptor(key, key),
+                    CryptoStreamMode.Write);
 
-            FileStream fsIn = new FileStream(inputFile, FileMode.Open);
+                FileStream fsIn = new FileStream(inputFile, FileMode.Open);
 
-            int data;
-            while ((data = fsIn.ReadByte()) != -1)
-                cs.WriteByte((byte)data);
+                int data;
+                while ((data = fsIn.ReadByte()) != -1)
+                    cs.WriteByte((byte)data);
 
 
-            fsIn.Close();
-            cs.Close();
-            fsCrypt.Close();
-            MessageBox.Show("Your file is now encrypted. Note: hide you password somewhere safe", "Encrypter", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                fsIn.Close();
+                cs.Close();
+                fsCrypt.Close();
+                File.Delete(textBox1.Text);
+                MessageBox.Show("Your file is now encrypted. Note: hide you password somewhere safe", "Encrypter", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
         }
+
 
         private void button3_Click(object sender, EventArgs e)
         {
             string inputFile = textBox1.Text;
-            string outputFile = textBox3.Text + textBox4.Text;
+            string outputFile = "Decrypted" + textBox4.Text;
             string password = textBox2.Text;
+            if (File.Exists(textBox1.Text))
+            {
+                UnicodeEncoding UE = new UnicodeEncoding();
+                byte[] key = UE.GetBytes(password);
 
-            UnicodeEncoding UE = new UnicodeEncoding();
-            byte[] key = UE.GetBytes(password);
+                FileStream fsCrypt = new FileStream(inputFile, FileMode.Open);
 
-            FileStream fsCrypt = new FileStream(inputFile, FileMode.Open);
+                RijndaelManaged RMCrypto = new RijndaelManaged();
 
-            RijndaelManaged RMCrypto = new RijndaelManaged();
+                CryptoStream cs = new CryptoStream(fsCrypt,
+                    RMCrypto.CreateDecryptor(key, key),
+                    CryptoStreamMode.Read);
 
-            CryptoStream cs = new CryptoStream(fsCrypt,
-                RMCrypto.CreateDecryptor(key, key),
-                CryptoStreamMode.Read);
+                FileStream fsOut = new FileStream(outputFile, FileMode.Create);
 
-            FileStream fsOut = new FileStream(outputFile, FileMode.Create);
+                int data;
+                while ((data = cs.ReadByte()) != -1)
+                    fsOut.WriteByte((byte)data);
 
-            int data;
-            while ((data = cs.ReadByte()) != -1)
-                fsOut.WriteByte((byte)data);
-
-            fsOut.Close();
-            cs.Close();
-            fsCrypt.Close();
-            MessageBox.Show("Your file has been decrypted.", "Encrypter", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                fsOut.Close();
+                cs.Close();
+                fsCrypt.Close();
+                File.Delete(textBox1.Text);
+                MessageBox.Show("Your file has been decrypted.", "Encrypter", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else
+            {
+                MessageBox.Show("File " + textBox1.Text + " does not exists", "Encrypter", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
         }
 
         private void button4_Click(object sender, EventArgs e)
