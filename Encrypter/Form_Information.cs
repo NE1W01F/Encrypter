251b7855455cf419ffe68c53e577d42b242deaa6
Wolf <36646784+NE1W01F@users.noreply.github.com>
2018-06-27 03:43:47 +0100

    Fixed All Bugs

---
+++ Encrypter/Form_Information.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Encrypter
+{
+    public partial class Form_Information : Form
+    {
+        public Form_Information()
+        {
+            InitializeComponent();
+        }
+
+        private void Cancel_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+    }
+}
